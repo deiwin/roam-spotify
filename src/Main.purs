53535205ad76093d4f867236e9e9cfb33f15b760
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2022-03-05 15:42:52 +0200

    Log warning when program fails
    Currently the program just stopped, with no logs or anything, because it
    waited infinitely for the fiber to complete before returning the
    failure.

--- src/Main.purs
+++ src/Main.purs
@@ -4,13 +4,13 @@ import Prelude
 import Control.Monad.Except (ExceptT, runExceptT, mapExceptT)
 import Control.Monad.Reader (ReaderT, runReaderT)
 import Control.Monad.Trans.Class (lift)
+import Data.Either (Either(..))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_, joinFiber)
-import Effect.Aff.Class (liftAff)
 import Effect.Class (liftEffect)
-import Effect.Console (logShow)
 import Roam (getFocusedBlockMetadata)
+import Effect.Console (log, logShow, warn)
 import Spotify (togglePlayback, withToken, Config(..), Env)
 
 config :: Config
@@ -31,7 +31,12 @@ main =
 
 runProgram :: forall a. Discard a => ReaderT Env (ExceptT String Aff) a -> Effect Unit
 runProgram program =
-  (launchAff_ <<< runExceptT) do
-    (Tuple env infiniteTokenRefreshFiber) <- withToken config
-    runReaderT program env
-    void $ liftAff $ joinFiber infiniteTokenRefreshFiber
+  launchAff_ do
+    result <-
+      runExceptT do
+        (Tuple env infiniteTokenRefreshFiber) <- withToken config
+        runReaderT program env
+        pure infiniteTokenRefreshFiber
+    case result of
+      Left e -> liftEffect $ warn ("Program failed with: " <> e)
+      Right fiber -> void $ joinFiber fiber
