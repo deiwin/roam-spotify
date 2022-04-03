11cfeddad5f0ad6f812811185552cefcbc30d2b4
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2022-04-03 14:36:07 +0300

    Experiment with coroutines for keyboard events
    The simple flow of reacting to individual keyboard events works, but the
    coroutines don't seem to be very flexible for building more complex
    things like accumulators or filtering.

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,10 @@
 module Main where
 
 import Prelude
+import Control.Coroutine (Producer)
+import Control.Coroutine as CR
+import Control.Coroutine.Aff (produce', emit)
+import Control.Coroutine.Aff as CRA
 import Control.Monad.Error.Class (throwError, catchError)
 import Control.Monad.Except (ExceptT, mapExceptT, runExceptT)
 import Control.Monad.Reader (ReaderT, runReaderT)
@@ -9,10 +13,22 @@ import Data.Either (Either(..))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_, joinFiber)
-import Effect.Class (liftEffect)
-import Effect.Console (warn)
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Console (warn, log)
 import Roam (updateFocusedBlockString)
 import Spotify (getPlaybackState, timestamp, togglePlayback, withToken, Config(..), Env)
+import Web.Event.Event (Event, preventDefault)
+import Web.Event.EventTarget (eventListener, addEventListener)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toEventTarget)
+import Web.HTML.Window (document)
+import Web.UIEvent.KeyboardEvent.EventTypes (keydown)
+import Web.UIEvent.KeyboardEvent (KeyboardEvent, fromEvent, key, toEvent)
+import Data.Maybe (Maybe(..))
+import Control.Monad.Rec.Class (forever)
+import Control.Safely (replicateM_)
+import Data.List (List(Nil), (:), take)
 
 config :: Config
 config =
@@ -25,9 +41,38 @@ config =
 main :: Effect Unit
 main =
   runProgram do
-    togglePlayback
-    togglePlayback
-    prefixFocusedBlockWithCurTimestamp
+    -- togglePlayback
+    -- togglePlayback
+    -- prefixFocusedBlockWithCurTimestamp
+    CR.runProcess $ keydownProducer `CR.connect` consumer
+    liftEffect $ log "done"
+  where
+  consumer :: CR.Consumer KeyboardEvent (ReaderT Env (ExceptT String Aff)) Unit
+  consumer =
+    forever do
+      keyboardEvent <- CR.await
+      liftEffect $ preventDefault $ toEvent keyboardEvent
+      -- lift $ prefixFocusedBlockWithCurTimestamp
+      liftEffect $ log (key keyboardEvent)
+
+takeFive :: CR.CoTransformer String (List String) Aff Unit
+takeFive = go Nil
+  where
+  go acc = do
+    o <- CR.cotransform acc
+    lift (liftEffect (log o))
+    go (take 5 (o : acc))
+
+keydownProducer :: forall m. MonadAff m => Producer KeyboardEvent m Unit
+keydownProducer =
+  produce' \emitter -> do
+    target <- window >>= document <#> toEventTarget
+    listener <- liftEffect $ eventListener (emitKeyboardEvent emitter)
+    liftEffect $ addEventListener keydown listener false target
+  where
+  emitKeyboardEvent emitter event = case fromEvent event of
+    Nothing -> pure unit
+    Just keyboardEvent -> emit emitter keyboardEvent
 
 prefixFocusedBlockWithCurTimestamp :: ReaderT Env (ExceptT String Aff) Unit
 prefixFocusedBlockWithCurTimestamp = do
